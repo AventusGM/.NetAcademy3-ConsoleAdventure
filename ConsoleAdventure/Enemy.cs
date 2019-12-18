2e8f217d967491caf010dc5fb4df05e71842f557
Aventus <Aventus@ex.ua>
2019-12-18 21:59:52 +0200

    [1] Ініціалізація
    Поїхали

---
+++ ConsoleAdventure/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAdventure
+{
+    public class Enemy
+    {
+        Int32 Health;
+        String Name;
+
+        public Int32 health { get; set; }
+        public String name
+        { get; set; }
+        public Enemy()
+        {
+            Health = health;
+            Name = name;
+        }
+        void getDamage()
+        {
+            health -= 10;
+        }
+    }
+    public class BigEvilWolf : Enemy
+    {
+       
+    }
+}
---
+++ ConsoleAdventure/Location.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAdventure
+{
+    public class Location
+    {
+        public string Name { get; set; }
+        public int Difficulty { get; set; }
+        public string[] PossibleEnemies { get; set; }
+
+        public Location(string name, int difficulty, string[] possibleEnemies)
+        {
+            Name = name;
+            Difficulty = difficulty;
+            PossibleEnemies = possibleEnemies;
+        }
+
+        public string GetEnemyName()
+        {
+            Random random = new Random();
+            return PossibleEnemies[random.Next(PossibleEnemies.Length)];
+        }
+    }
+}
