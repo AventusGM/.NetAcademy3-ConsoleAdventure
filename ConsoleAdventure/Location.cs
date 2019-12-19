71ec7f991e24643a5d1366ba1673639152d661af
Na21k <hulevych@hotmail.com>
2019-12-20 01:31:05 +0200

    big changes in Location and Player(added inventory and methods)

--- ConsoleAdventure/Location.cs
+++ ConsoleAdventure/Location.cs
@@ -6,13 +6,66 @@ using System.Threading.Tasks;
 
 namespace ConsoleAdventure
 {
+    public struct Coords
+    {
+        public uint x;
+        public uint y;
+
+        public Coords(uint x, uint y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
     public class Location
     {
-        public string Name { get; set; }
-        public int Difficulty { get; set; }
-        public string[] PossibleEnemies { get; set; }
+        public string Title { get; set; }
+        private uint size;
+        //Player player { set; get; }?
+        //Enemy Enemy { set; get; }?
 
-        public Location(string name, int difficulty, string[] possibleEnemies)
+        protected Location()
+        {
+            Title = null;
+            size = 0;
+        }
+
+        public Location(string titlle, uint size)
+        {
+            Title = titlle;
+            this.size = size;
+
+            //generate coords for player and enemy?
+        }
+
+        public Coords GeneratePlayerCoords()
+        {
+            Random rand = new Random((int)size - 1);
+
+            Coords playerCoords = new Coords((uint)rand.Next(), (uint)rand.Next());
+
+            return playerCoords;
+        }
+
+        public Coords GenerateEnemyCoords(Coords playerCoords)
+        {
+            Random rand = new Random((int)size - 1);
+
+            Coords enemyCoords;
+
+            do
+            {
+                enemyCoords = new Coords((uint)rand.Next(), (uint)rand.Next());
+            } while (enemyCoords.x == playerCoords.x && enemyCoords.y == playerCoords.y);
+
+            return enemyCoords;
+        }
+
+        /*public int Difficulty { get; set; }
+        public string[] PossibleEnemies { get; set; }*/
+
+        /*public Location(string name, int difficulty, string[] possibleEnemies)
         {
             Name = name;
             Difficulty = difficulty;
@@ -23,6 +76,8 @@ namespace ConsoleAdventure
         {
             Random random = new Random();
             return PossibleEnemies[random.Next(PossibleEnemies.Length)];
-        }
+        }*/
+
+
     }
 }
--- ConsoleAdventure/Player.cs
+++ ConsoleAdventure/Player.cs
@@ -9,40 +9,50 @@ namespace ConsoleAdventure
 	public class Player : Creature
 	{
 		public uint Armor { private set; get; }
-		public Weapon Weapon { set; get; }
+		public Weapon Weapon
+		{
+			set
+			{
+				Weapon = value;
 
-        //! add inventory using Inventory class
+				if (!Inventory.Items.Contains(value))
+					AddToInventory(value);
+			}
+			get => Weapon;
+		}
+		public Inventory Inventory { private set; get; }
+		public uint InventoryCurrent { private set; get; }
 
 		public Player()
 		{
 			Name = null;
 			Health = 100;
 			Armor = 0;
-			Weapon = null;
+			Inventory = new Inventory();
+			Inventory.Items.Add(Weapons.Sword);
+			InventoryCurrent = 0;
+			Weapon = (Weapon)Inventory.Items[0];
 		}
 
-		public Player(string name, Weapon weapon)
+		public Player(string name) : this()
 		{
 			Name = name;
-			Health = 100;
-			Armor = 0;
-			Weapon = weapon;
 		}
 
 		public void Run()
 		{
-			Random rand = new Random(1);
+			Random rand = new Random(2);
 
-			int damage = rand.Next();
+			uint damageChance = (uint)rand.Next();
 
-			if (damage == 1)
-				Health -= damage;
+			if (damageChance == 0)
+				GetDamage(10);
 		}
 
-		public override void Hurt(Creature e)
+		public override void Hurt(Creature whoToHurt)
 		{
-			if (e.GetDamage(Weapon.Damage) <= 0)
-				Health = 100;
+			if (whoToHurt.GetDamage(Weapon.Damage) <= 0)
+				Heal(25);
 		}
 
 		public override int GetDamage(uint damage)
@@ -76,6 +86,52 @@ namespace ConsoleAdventure
 		public void GetArmor(uint armor)
 		{
 			Armor += armor;
+		}
+
+		public Item AddToInventory(Item item)
+		{
+			Inventory.Items.Add(item);
+
+			return item;
+		}
+
+		public bool RmCurrentFromInventory()
+		{
+			if (Inventory.Items.Count > 0)
+			{
+				Inventory.Items.RemoveAt((int)InventoryCurrent);
+				return true;
+			}
+			else
+			{
+				Console.WriteLine("can not remove the only element from the inventory");
+				return false;
+			}
+		}
+
+		public bool SelectInventoryItem(uint index)
+		{
+			if (index < Inventory.Items.Count)
+			{
+				InventoryCurrent = index;
+
+				if (Inventory.Items[(int)InventoryCurrent].GetType() == typeof(Weapon))
+					Weapon = (Weapon)Inventory.Items[(int)InventoryCurrent];
+
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		public void UseCurrentItem()
+		{
+			if (Inventory.Items[(int)InventoryCurrent].GetType() == typeof(Weapon))
+				Console.WriteLine("this is weapon, you can only hurt someone with it");
+			else
+				Inventory.Items[(int)InventoryCurrent].Use();
 		}
 	}
 }
